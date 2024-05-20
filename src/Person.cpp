64b664f6008594f2a884c6aa23b6abeb77c0781c
Amshu Bellur <abell062@xe-01.cs.ucr.edu>
2024-05-19 18:40:56 -0700

    added a few comments

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,5 +1,6 @@
 #include "../header/Person.h"
 
+//constructor for the person class
 Person::Person(string characterName, string characterWeakness, string characterStrength){
     characterName = characterName;
     weakness = characterWeakness;
@@ -9,26 +10,33 @@ Person::Person(string characterName, string characterWeakness, string characterS
     gameProgressGauge = 0;
 }
 
+//update the status of the gun
 void Person::updateGun(bool status){
     gun = status;
 }
 
+//update the status of the knife
 void Person::updateKnife(bool status){
     knife = status;
 }
 
+//return the chatacter's name 
 string Person::getName(){
     return (characterName);
 }
 
+//update the progress based on a a percentage scale
 void Person::updateProgress(int level){
     gameProgressGauge = level *20;
 }
 
+
+//return the strength of the person
 string Person::getStrength(){
     return(strength);
 }
 
+//return the weakness of the person
 string Person::getWeakness(){
     return(weakness);
 }
