c8ff78260948351ffeb00be4cb4272ca7231a002
Rikki Kahn-Yee <rkahn007@xe-01.cs.ucr.edu>
2024-05-21 20:08:46 -0700

    updated Game class event 1 function

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,10 +8,10 @@ void Game::event1(Person person, Display display){
     vector<string> options{"(W) Wait for the zombies to leave and then exit the janitors closet", "(S) Use the supplies in the closet as a weapon against the zombies",
      "(V) Use the vents in the closet to get out", "(J) Ask Jerome to go out and knock out all the zombies", 
      "(R) Ask Ryan to go talk to the zombies and manipulate them into letting you all go", 
-     "(C) Ask Cassidy to lift throw heavy buckets from the closest onto the zombies", "(Z) Use your skills against the zombies";}
+     "(C) Ask Cassidy to lift throw heavy buckets from the closest onto the zombies", "(Z) Use your skills against the zombies"};
 
     string event1 = "When you begin to run out of the classroom and into the hallway you notice a hoard of zombies. You are backed against a corner. You notice that you are backed against the janitors closet. You enter the closest with all your classmates to hide from the zombies. What do you want to do now?";
-    display.eventPompt(event1);
+    display.eventPrompt(event1);
     char option;
     if(person.getName() == "Greta"){
         display.options(options.at(0), options.at(1), options.at(6));
