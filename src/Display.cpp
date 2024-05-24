8b92bf7d8b663f98372828d6075855e7f666ba2b
Amshu Bellur <abell062@xe-01.cs.ucr.edu>
2024-05-23 22:04:23 -0700

    fixed parameter in the if loop for backstory function

--- src/Display.cpp
+++ src/Display.cpp
@@ -37,21 +37,21 @@ void Display::menu() {
     cout << "Please type the name of the character you would like to be: ";
 }
 void Display::backstory(Person person) {
-    if (person.getName() == "Greta" || person.getName() == "greta"){
+    if (person.getName() == "Greta"){
         cout << "Hi Greta! Hope you are ready for some tough challenges. A character skill that you carry is your ability to pick locks. When you were a child, your parents would lock your favorite candy in the top cabinet with a lock. With a lot of practice, you were able to figure out how to pick the lock and can pick any lock that you want now! Now you and the people in your class must exit the campus together to survive."<< endl;
-    } else if (person.getName() == "Jerome" || person.getName() == "jerome"){
+    } else if (person.getName() == "Jerome"){
         cout<< "Hi Jerome! Hope you are ready for some tough challenges. A character skill that you carry is your ability to knock people out with one punch. When you were a child, you were bullied a lot as a kid. Your parents signed you up for MMA classes to teach you self defense, and now you can really fight! Now you and the people in your class must exit the campus together to survive." << endl;
         cout << endl;
-    } else if (person.getName() == "Cassidy" || person.getName() == "cassidy"){
+    } else if (person.getName() == "Cassidy"){
         cout<< "Hi Cassidy! Hope you are ready for some tough challenges. A character skill that you carry is your strength! When you were a child, you would work in the ice cream parlor every summer to make some money and with every scoop that you scooped, you gained a lot of muscle. Now you can bench over 250lbs! Now you and the people in your class must exit the campus together to survive." << endl;
         cout << endl;
-    } else if (person.getName() == "Dr.Brown" || person.getName() == "dr.brown" || person.getName() == "Dr.brown" || person.getName() == "dr.Brown"){
+    } else if (person.getName() == "Dr.Brown"){
         cout<< "Hi Dr.Brown! Hope you are ready for some tough challenges. A character skill that you carry is your glasses! When you were a child, you would play with your glasses and the sun because you could start fires with the right angle and shine light in other people’s faces! However, you are a little bit out of practice since you have not played with your glasses since you became a professor. Now you and the people in your class must exit the campus together to survive." << endl;
         cout << endl;
-    } else if (person.getName() == "Jessie" || person.getName() == "jessie"){
+    } else if (person.getName() == "Jessie"){
         cout<< "Hi Jessie! Hope you are ready for some tough challenges. A character skill that you carry is your speed! When you were a child, you did not have a lot of money growing up, so you would steal cans of soup from the supermarket and run back home to make sure that you were not caught by the guards. With your practice in running, you were able to become a track star and set the school record for fastest sprinter. Now you and the people in your class must exit the campus together to survive." << endl;
         cout << endl;
-    } else if (person.getName() == "Ryan" || person.getName() == "ryan"){
+    } else if (person.getName() == "Ryan"){
         cout<< "Hi Ryan! Hope you are ready for some tough challenges. A character skill that you carry is your ability to manipulate people! When you were a child, your parents got divorced and got you a lot of gifts to make up for it. Eventually you learned to manipulate them to get any gift that you wanted. With this skill of manipulation, you became very popular in school and learned how to manipulate the people in school! Now you and the people in your class must exit the campus together to survive." << endl;
         cout << endl;
     }
