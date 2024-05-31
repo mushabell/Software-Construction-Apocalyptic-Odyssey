7259dd6b6099459b02d3ca726e2a1f461743a150
Amshu Bellur <abell062@xe-01.cs.ucr.edu>
2024-05-31 15:38:48 -0700

    figured out the testing

--- tests/test.cpp
+++ tests/test.cpp
@@ -111,42 +111,42 @@ TEST(Backstory, testRyan) {
     EXPECT_EQ(outStreamOne.str(), expectedBackstory);
 }
 
-// TEST(GameTests, testEvent3_Greta) {
-//     Game game;
-//     Person person("Greta", "can not see without glasses", "good lock picker");
-//     Display display;
+TEST(GameTests, testEvent3_Greta) {
+    Game game;
+    Person person("Greta", "can not see without glasses", "good lock picker");
+    Display display;
 
-//     // Simulate user input for option 'C'
-//     std::istringstream input("C\n");
-//     std::cin.rdbuf(input.rdbuf());
+    // Simulate user input for option 'C'
+    std::istringstream input("B\n");
+    std::cin.rdbuf(input.rdbuf());
 
-//     // Capture the output
-//     std::ostringstream output;
-//     std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
+    // Capture the output
+    std::ostringstream output;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(output.rdbuf());
 
-//     // Execute the event3 function
-//     game.event3(person, display);
+    // Execute the event3 function
+    game.event3(person, display);
 
-//     // Restore the original buffer for std::cout
-//     std::cout.rdbuf(oldCoutBuffer);
+    // Restore the original buffer for std::cout
+    std::cout.rdbuf(oldCoutBuffer);
 
-//     // Verify the prompt
-//     std::string expectedPrompt = "Nice choice! You made it outside of the building! You decide to go through the cafeteria to choose the shortest path to get off the campus as fast as possible. When you enter the cafeteria you see that the building is empty. You and your classmates enter the main hall and the door closes behind you. You notice a knife on the floor and pick it up for protection. You walk across the hall and try to open the door behind you but you can not! What do you want to do now?\n";
-//     EXPECT_TRUE(output.str().find(expectedPrompt) != std::string::npos);
+    // Verify the prompt
+    std::string expectedPrompt = "Nice choice! You made it outside of the building! You decide to go through the cafeteria to choose the shortest path to get off the campus as fast as possible. When you enter the cafeteria you see that the building is empty. You and your classmates enter the main hall and the door closes behind you. You notice a knife on the floor and pick it up for protection. You walk across the hall and try to open the door behind you but you can not! What do you want to do now?\n";
+    EXPECT_TRUE(output.str().find(expectedPrompt) != std::string::npos);
 
-//     // Verify the options displayed
-//     std::vector<std::string> expectedOptions = {
-//         "(B) Use the butter in the cafeteria to make the door lock slippery",
-//         "(C) Ask Cassidy to break the door down",
-//         "(S) Use your skill against the door"
-//     };
-//     for (const auto& option : expectedOptions) {
-//         EXPECT_TRUE(output.str().find(option) != std::string::npos);
-//     }
+    // Verify the options displayed
+    std::vector<std::string> expectedOptions = {
+        "(B) Use the butter in the cafeteria to make the door lock slippery",
+        "(C) Ask Cassidy to break the door down",
+        "(S) Use your skill against the door"
+    };
+    for (const auto& option : expectedOptions) {
+        EXPECT_TRUE(output.str().find(option) != std::string::npos);
+    }
 
-//     // Verify the person has the knife weapon
-//     std::vector<std::string> expectedWeapons = {"knife"};
-//     std::vector<std::string> actualWeapons;
-//     actualWeapons.push_back("knife");
-//     EXPECT_EQ(actualWeapons, expectedWeapons);
-// }
+    // Verify the person has the knife weapon
+    std::vector<std::string> expectedWeapons = {"knife"};
+    std::vector<std::string> actualWeapons;
+    actualWeapons.push_back("knife");
+    EXPECT_EQ(actualWeapons, expectedWeapons);
+}
